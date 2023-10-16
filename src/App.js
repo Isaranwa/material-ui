102984ea9defb6ebc17810c8ff5c42738842a96f
Isaranwa <pennyisaranwa@gmail.com>
2023-10-16 20:42:33 +0300

    Add the components to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Rightbar } from "./components/Rightbar";
+import { Sidebar } from "./components/Sidebar";
+import { Feeds } from "./components/Feeds";
+import { Box, Stack } from "@mui/material";
+
 function App() {
-  return <div></div>;
+  return (
+    <Box>
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar />
+        <Feeds />
+        <Rightbar />
+      </Stack>
+    </Box>
+  );
 }
 
 export default App;
---
+++ src/components/Rightbar.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+export const Rightbar = () => {
+  return (
+    <Box bgcolor="red" flex={2} p={2}>
+      Rightbar
+    </Box>
+  );
+};
