8449cee7f3da3138adc0f09411c4cd7f3b887c6c
Isaranwa <pennyisaranwa@gmail.com>
2023-10-21 13:36:31 +0300

    Create custom style for the Modal

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,11 +1,19 @@
-import { Fab, Tooltip } from "@mui/material";
+import { Box, Fab, Modal, Tooltip, Typography, styled } from "@mui/material";
 import { Add as AddIcon } from "@mui/icons-material";
-import React from "react";
+import React, { useState } from "react";
+
+const StyledModal = styled(Modal)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
 
 export const Add = () => {
+  const [open, setOpen] = useState(false);
   return (
     <>
       <Tooltip
+        onClick={(e) => setOpen(true)}
         title="Add post"
         sx={{
           position: "fixed",
@@ -17,6 +25,18 @@ export const Add = () => {
           <AddIcon />
         </Fab>
       </Tooltip>
+      <StyledModal
+        open={open}
+        onClose={(e) => setOpen(true)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box width={400} height={280} p={3} bgcolor="white" borderRadius={5}>
+          <Typography variant="h6" color={"grey"} textAlign={"center"}>
+            Create New Post
+          </Typography>
+        </Box>
+      </StyledModal>
     </>
   );
 };
