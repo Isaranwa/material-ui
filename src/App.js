f41e0f718ad0c7657ccd5d87e04477ddbeabe3ae
Isaranwa <pennyisaranwa@gmail.com>
2023-10-21 14:18:39 +0300

    Created a dark theme mode

--- src/App.js
+++ src/App.js
@@ -2,20 +2,30 @@ import { Rightbar } from "./components/Rightbar";
 import { Sidebar } from "./components/Sidebar";
 import { Navbar } from "./components/Navbar";
 import { Feeds } from "./components/Feeds";
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, ThemeProvider, createTheme } from "@mui/material";
 import { Add } from "./components/Add";
+import { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("dark");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
   return (
-    <Box>
-      <Navbar />
-      <Stack direction="row" spacing={2} justifyContent="space-between">
-        <Sidebar />
-        <Feeds />
-        <Rightbar />
-      </Stack>
-      <Add />
-    </Box>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar />
+          <Feeds />
+          <Rightbar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
   );
 }
 
