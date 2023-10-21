e50e9daa0783e024ad3b9cf0e73623cb5048126d
Isaranwa <pennyisaranwa@gmail.com>
2023-10-21 13:20:41 +0300

    Change position of Add button on small devices

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,5 +1,22 @@
+import { Fab, Tooltip } from "@mui/material";
+import { Add as AddIcon } from "@mui/icons-material";
 import React from "react";
 
 export const Add = () => {
-  return <div>Add</div>;
+  return (
+    <>
+      <Tooltip
+        title="Add post"
+        sx={{
+          position: "fixed",
+          bottom: 20,
+          left: { xs: "calc(50% - 25px)", md: 30 },
+        }}
+      >
+        <Fab color="primary" aria-label="add">
+          <AddIcon />
+        </Fab>
+      </Tooltip>
+    </>
+  );
 };
